137b92958b49093d46a458bd9139f06028f1d4c3
malcomowilla <owillamalcom@gmail.com>
2023-04-26 16:17:02 +0300

    solved

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+
+
+function superbowlWin(array_objects) {
+    for (const record of array_objects) {
+        if (record.result === "W") {
+            return record.year
+        }
+    }
+}
+
