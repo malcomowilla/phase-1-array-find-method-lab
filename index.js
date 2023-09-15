93dfc9d33c36d66ccfce17659ba78d1dbb2a47b9
malcomowilla <owillamalcom@gmail.com>
2023-09-15 14:52:35 +0300

    array iterator methods

--- index.js
+++ index.js
@@ -2,11 +2,46 @@
 
 
 
-function superbowlWin(array_objects) {
-    for (const record of array_objects) {
-        if (record.result === "W") {
-            return record.year
-        }
+// function superbowlWin(array_objects) {
+//     for (const record of array_objects) {
+//         if (record.result === "W") {
+//             return record.year
+//         }
+//     }
+// }
+
+
+
+
+
+function superbowlWin( array) {
+    const finding = array.find((element)=>element.result === 'W' )
+
+    if (finding) {
+        return finding.year
+    } else {
+        return undefined
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
